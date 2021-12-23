c08cd2c47c02293bb78189593a918fd9000e96f2
gustavoschaefer <gustavo.schaefer@mercadolivre.com>
2021-12-23 18:46:42 -0300

    Ajustes finos

--- pratica1/src/main/java/com/example/praticas/controller/AlunoController.java
+++ pratica1/src/main/java/com/example/praticas/controller/AlunoController.java
@@ -9,13 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @RestController
 public class AlunoController {
@@ -23,17 +20,16 @@ public class AlunoController {
     @Autowired
     private AlunoService alunoService;
 
-    private static List<Aluno> alunos =
+    private static final List<Aluno> alunos =
         Arrays.asList(
-            Aluno.builder().matricula(1).nome("Aluno1").notas(Map.of("Aula1", Integer.valueOf(9),"Aula2", Integer.valueOf(8))).build(),
-            Aluno.builder().matricula(2).nome("Aluno2").notas(Map.of("Aula1", Integer.valueOf(10),"Aula2", Integer.valueOf(9))).build(),
-            Aluno.builder().matricula(3).nome("Aluno3").notas(Map.of("Aula1", Integer.valueOf(9),"Aula2", Integer.valueOf(10))).build()
+            Aluno.builder().matricula(1).nome("Aluno1").notas(Map.of("Aula1", 9,"Aula2", 8)).build(),
+            Aluno.builder().matricula(2).nome("Aluno2").notas(Map.of("Aula1", 10,"Aula2", 9)).build(),
+            Aluno.builder().matricula(3).nome("Aluno3").notas(Map.of("Aula1", 9,"Aula2", 10)).build()
         );
 
     @GetMapping("/alunos")
     private List<Aluno> getList() {
-        List<Aluno> lista = alunos.stream().collect(Collectors.toList());
-        return lista;
+        return new ArrayList<>(alunos);
     }
 
     @PostMapping("/aluno")
@@ -49,7 +45,7 @@ public class AlunoController {
     public ResponseEntity<Diploma> getDiploma(@RequestBody AlunoDto alunoDto) {
         Aluno aluno = AlunoDto.toAluno(alunoDto);
         float media = alunoService.media(aluno);
-        String msg = null;
+        String msg;
         if (media >= 9) {
             msg = "Parabéns, passou com mádia maior do que 9!";
         } else {
